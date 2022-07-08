088ff85e1769a9e6060e4088062f6d8ac6a02313
TarkeshwarKhapekar <93650602+TarkeshwarKhapekar@users.noreply.github.com>
2022-07-08 23:58:21 +0530

    Add files via upload

---
+++ Register/register/README.md
@@ -0,0 +1,2 @@
+# Firebase-CRUD
+This will demonstrate the CRUD operation to be performed on Firebase using HTML,JS and CSS.
---
+++ Register/register/firebase connectivity.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDfXhsD_0MvJMN-s-RJNVr67XMxrSF_Prw",
+    authDomain: "sql1-661c6.firebaseapp.com",
+    projectId: "sql1-661c6",
+    storageBucket: "sql1-661c6.appspot.com",
+    messagingSenderId: "360172645189",
+    appId: "1:360172645189:web:fd2b0a949a608414e6ac2c",
+    measurementId: "G-2ESBEG4GGP"
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
